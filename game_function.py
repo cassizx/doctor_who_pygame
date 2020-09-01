d23e56885c72c13fcebf5ddedc8b388c31a51e26
CassiZX <alexkvzx@gmail.com>
2020-09-01 20:17:51 +0300

    Функция check_events поделена на 3 функции: check_events, check_keyup_events , check_keydown_events

--- game_function.py
+++ game_function.py
@@ -1,30 +1,41 @@
 import sys
 import pygame
 
+
+def check_keydown_events(event, ship):
+    '''Регестрирует нажатие клавиш.'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+    elif event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+
+def check_keyup_events(event, ship):
+    '''Реагирует на отпускание клавиш.'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+    elif event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
 def check_events(ship):
+    '''Обрабатывает нажатия клавиш и события мыши.'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-            elif event.key == pygame.K_UP:
-                ship.moving_up = True
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
-            elif event.key == pygame.K_UP:
-                ship.moving_up = False
-            elif event.key == pygame.K_DOWN:
-                ship.moving_down = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings, screen, ship, background):
+    '''При каждом проходе цикла перерисовывается экран '''
     screen.fill(ai_settings.bg_color)
     background.blitme()
     ship.blitme() 
